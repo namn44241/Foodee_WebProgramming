2d590a1efdf1b4ebc33dc8ba80ec588a88a7aa6b
namn44241 <nguyenvandoaaaaa@gmail.com>
2024-11-26 12:58:03 +0700

    [FIX-ADD] Dịch xong Trang chủ

--- mail.php
+++ mail.php
@@ -15,7 +15,7 @@
 	$subject = stripslashes( nl2br( $_POST['subject'] ) );
 	$message = stripslashes( nl2br( $_POST['message'] ) );
 	
-	$headers ="From: Form Contact <$from>\n";
+	$headers ="From: Form Thông tin liên hệ <$from>\n";
 	$headers.="MIME-Version: 1.0\n";
 	$headers.="Content-type: text/html; charset=iso 8859-1";
 	
